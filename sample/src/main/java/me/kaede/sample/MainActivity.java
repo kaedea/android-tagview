fdbdd90a0a88cc12c06a716c29e775ed12ff824b
kaede <kidhaibara@gmail.com>
2015-05-12 20:15:28 +0800

    edit sample

--- sample/src/main/java/me/kaede/sample/MainActivity.java
+++ sample/src/main/java/me/kaede/sample/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -195,4 +197,26 @@ public class MainActivity extends ActionBarActivity {
             tagView.addTag(tag);
         }
     }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_github) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
 }
